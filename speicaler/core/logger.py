98bf161f96eb81d4bdab53c952e1c4b19de537d8
H17W <ht_ma@icloud.com>
2026-09-13 21:49:28 +0300

    Delete speicaler directory

--- speicaler/core/logger.py
+++
@@ -1,8 +0,0 @@
-import logging, sys
-from .config import settings
-
-def setup_logging():
-    logging.basicConfig(level=getattr(logging, settings.log_level, logging.INFO), format='%(asctime)s | %(levelname)s | %(name)s | %(message)s', handlers=[logging.StreamHandler(sys.stdout)])
-    return logging.getLogger('special')
-
-log = setup_logging()
